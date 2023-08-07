42423a0c5b347b4f1d550f682a10f51be29de515
kdrbipul <kdrbipul@gmail.com>
2023-08-07 20:39:09 +0600

    last commit

--- post.js
+++ post.js
@@ -3,7 +3,22 @@
 function loadPosts(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayPost(data))
+}
+
+
+function displayPost(data){
+    const postArea = document.getElementById('post');
+    for(const post of data){
+        const div=document.createElement('div');
+        div.classList.add('make-up');
+        div.innerHTML=`
+        <h2>${post.title}</h2>
+        <h3>${post.id}</h3>
+        <p>${post.body}</p>
+        `;
+        postArea.appendChild(div);
+    }
 }
 
 loadPosts()
